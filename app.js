b2688238589929152e9e54a9ea599d19a8d0ee65
Caroline <caroline.fonseca0001@gmail.com>
2022-12-06 18:24:03 -0500

    Did Variable exercises

--- app.js
+++ app.js
@@ -4,7 +4,38 @@ console.log(
 );
 
 // Exercise 1
+const firstName = "Caroline"
+let lastName = "Fonseca";
+var age = 35;
 
 // Exercise 2
+lastName = "Fonseca";
+age = 35;
+
+console.log(`My name is ${firstName} ${lastName} and I am ${age} years old.`)
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive= true;
+
+console.log(`${language} was released in ${createdYear}`);
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log (`The price of the ${selectedSize} shirt is ${price}. Is it on sale? ${isOnSale}. How many are in stock? ${stock}.`)
+
+// Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss"
+let pageCount = 722;
+let bookmark = 456;
+let hasread = 'true';
+
+console.log(`The name of the book is "${title}" and it is by ${author}. I am on page ${bookmark} out of ${pageCount}.`)
